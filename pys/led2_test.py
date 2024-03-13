89ed7ed224fd53a21f01ff5abce69d77567e4f24
texsd <texsd.tt29@outlook.com>
2024-03-13 22:38:00 +0800

    update structure

---
+++ pys/led2_test.py
@@ -0,0 +1,16 @@
+import time
+from machine import Pin
+
+led=Pin(4,Pin.OUT)
+
+while True:
+    led.value(1)
+    print('start')
+    time.sleep(0.5)
+    led.value(0)
+    print('stop')
+    time.sleep(0.5)
+    
+
+
+
