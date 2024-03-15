98012058fe7b3cd135ae11c324d8446d2ae4ccf2
texsd <texsd.tt29@outlook.com>
2024-03-15 09:07:35 +0800

    update_rd

--- pys/rain_detector/rain_detector.py
+++ pys/rain_detector/rain_detector.py
@@ -1,6 +1,16 @@
 from umqtt.simple import MQTTClient
 from machine import Pin
 import time
+import network
+
+# 配网
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+# sta_if.scan()
+sta_if.connect("k30su", "123456879")
+while not sta_if.isconnected() == 0:
+    print("connecting.")
+print(sta_if.ifconfig())
 
 # MQTT服务器的IP地址
 mqtt_server = 'v.texsd.eu.org'
@@ -29,7 +39,15 @@ def send_mqtt(client, topic, message):
 
 mqtt_client = connect_mqtt()
 
+# 初始化上一次的雨滴状态
+last_rain_status = None
+
 while True:
     rain_status = check_rain(rain_sensor)
-    send_mqtt(mqtt_client, mqtt_topic, rain_status)
-    time.sleep(5)  # wait for 5 seconds
+    # 检查雨滴状态是否发生变化
+    if rain_status != last_rain_status:
+        send_mqtt(mqtt_client, mqtt_topic, rain_status)
+        # 更新上一次的雨滴状态
+        last_rain_status = rain_status
+    time.sleep(5)  # 5s后重新探测
+
